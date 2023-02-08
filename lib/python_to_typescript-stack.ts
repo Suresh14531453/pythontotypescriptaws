de1f400d4af2c170cde614720f8f1b32c1abc003
sureshsahu <suresh.sahu@trangile.com>
2023-02-08 21:34:47 +0530

    firstcommit

--- lib/python_to_typescript-stack.ts
+++ lib/python_to_typescript-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,9 @@ export class PythonToTypescriptStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PythonToTypescriptQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket=new Bucket(this,"myfirstbucketpythontotypescript",{
+versioned:true,
+encryption:BucketEncryption.KMS_MANAGED,
+    })
   }
 }

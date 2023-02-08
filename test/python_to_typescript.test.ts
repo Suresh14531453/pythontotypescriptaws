5a39d5981b2c503e2d731807514344d5915cfd27
sureshsahu <suresh.sahu@trangile.com>
2023-02-08 20:43:26 +0530

    Initial commit

---
+++ test/python_to_typescript.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PythonToTypescript from '../lib/python_to_typescript-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/python_to_typescript-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PythonToTypescript.PythonToTypescriptStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
